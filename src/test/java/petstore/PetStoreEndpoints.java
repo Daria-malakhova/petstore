0ba1678342f775348e83280847c3d9aab7526ca1
Daria Malakhova <41268261+Daria-malakhova@users.noreply.github.com>
2020-12-21 22:47:29 +0300

    Update PetStoreEndpoints.java

--- src/test/java/petstore/PetStoreEndpoints.java
+++ src/test/java/petstore/PetStoreEndpoints.java
@@ -4,7 +4,18 @@ public enum PetStoreEndpoints {
   PET_UPLOAD_IMAGE("/pet/%s/uploadImage"),
   PET("/pet"),
   PET_BY_ID("/pet/%s"),
-  PET_FIND_BY_STATUS("/pet/findByStatus");
+  PET_FIND_BY_STATUS("/pet/findByStatus"),
+
+  STORE("/store/order"),
+  STORE_BY_ID("/store/order/%s"),
+  STORE_INVENTORY("/store/inventory"),
+
+  USER_BY_USERNAME("/user/%s"),
+  USER_CREATE_WITH_ARRAY("/user/createWithArray"),
+  USER_CREATE_WITH_LIST("/user/createWithList"),
+  USER_LOGIN("/user/login"),
+  USER_LOGOUT("/user/logout"),
+  USER("/user");
 
   private final String url;
 
